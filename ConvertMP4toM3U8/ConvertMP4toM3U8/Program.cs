5abaf31819dd77599a920c4f1950f8d4aca4d64f
ilyasaruca <ilyasaruca@gmail.com>
2020-01-12 15:25:35 +0300

    Project Completed

---
+++ ConvertMP4toM3U8/ConvertMP4toM3U8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMP4toM3U8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+
+            ffMpeg.GetVideoThumbnail("input file path", "output_thumbnail_file.jpg");
+
+            Console.Write("Thumbnail Created."); 
+        }
+    }
+}
